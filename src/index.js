3193432173160c0ab13ac64a97f52a840a23f00c
jjeon17 <33076677+Jonathan940110@users.noreply.github.com>
2021-11-15 20:13:41 +0900

    Change Routes.js to Router.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import GlobalStyle from './styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
-import Routes from './Routes';
+import Routes from './Router';
 
 ReactDOM.render(
   <>
