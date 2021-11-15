83682d3f0b750c67db6fb248b1f8722b6e7e8c84
jjeon17 <33076677+Jonathan940110@users.noreply.github.com>
2021-11-15 18:35:43 +0900

    Edit: 초기세팅 폰트 스타일 수정

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -3,12 +3,11 @@ import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
-  @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
-  
   body{
     font-family: 'Roboto', sans-serif;
   }
-  
+
+
 `;
 
 export default GlobalStyle;
