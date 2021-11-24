b002ffaff166e6725faf7bf85edbe11ae9b010b6
woncheolok <okcleff@gmail.com>
2021-11-24 20:58:12 +0900

    Add: [Main] 리뷰 반영

--- src/Router.js
+++ src/Router.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
 import ProductDetail from './pages/ProductDetail/ProductDetail';
 import Shop from './pages/Shop/Shop';
-import Nav from './components/Nav';
+import Nav from './components/Nav/Nav';
 import Footer from './components/Footer';
 import Login from './pages/LoginRegister/Login';
 
@@ -10,16 +10,13 @@ function Router() {
   return (
     <BrowserRouter>
       <Nav />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/users/signin" element={<Main />} />
-          {/* <Route path="/main" element={<Redirect />} /> */}
-          <Route path="/detail" element={<ProductDetail />} />
-          <Route path="/products" element={<Shop />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/users/signin" element={<Main />} />
+        <Route path="/detail" element={<ProductDetail />} />
+        <Route path="/products" element={<Shop />} />
+      </Routes>
       <Footer />
     </BrowserRouter>
   );
---
+++ src/config.js
@@ -0,0 +1,6 @@
+const BASE_URL = 'http://10.58.3.37:8000';
+
+export const API = {
+  baseUrl: `${BASE_URL}`,
+  sortBy: `${BASE_URL}/products?sort=`,
+};
