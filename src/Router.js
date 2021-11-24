bbf3f6f637f3651bb3e5de173121bd9f76c46385
woncheolok <okcleff@gmail.com>
2021-11-24 20:47:08 +0900

    Router.js 충돌 해결

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 import Main from './pages/Main/Main';
 import ProductDetail from './pages/ProductDetail/ProductDetail';
 import Shop from './pages/Shop/Shop';
-import Order from './pages/Order/Order';
-import Modal from './pages/ProductDetail/components/Modal';
-import Nav from './components/Nav/Nav';
+import Nav from './components/Nav';
 import Footer from './components/Footer';
 import Login from './pages/LoginRegister/Login';
-import Redirect from './components/KakaoLogin/Redirect';
 
 function Router() {
   return (
@@ -16,15 +12,12 @@ function Router() {
       <Nav />
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Main />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/main" element={<Main />} />
-          <Route path="/main" element={<Redirect />} />
-          {/* <Route path="/users/signin" element={<Redirect />} /> */}
+          <Route path="/users/signin" element={<Main />} />
+          {/* <Route path="/main" element={<Redirect />} /> */}
           <Route path="/detail" element={<ProductDetail />} />
           <Route path="/products" element={<Shop />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/order" element={<Order />} />
-          <Route path="/modal" element={<Modal />} />
         </Routes>
       </BrowserRouter>
       <Footer />
